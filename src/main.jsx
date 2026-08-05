facf5c03fed311b6fad018bb040ed51d4f3801db
RogueXPhantom <pulkitgautam7@gmail.com>
2026-08-05 07:58:13 +0530

    Configure custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/RogueX-Portfolio">
+    <BrowserRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
